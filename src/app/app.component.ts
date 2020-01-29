d4cc707f54254b1e75c1e18184092697ff2ef52f
iamharry <harender_24@hotmail.com>
2020-01-29 07:15:46 +0530

    First draft

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,107 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+  images = [
+    {
+      title: 'white boat on blue ocean water',
+      url: 'https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white canoe near palm tree',
+      url: 'https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'red and blue house near dock',
+      url: 'https://images.unsplash.com/photo-1490162190997-391cff4632e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'short-coated brown dog near seashore',
+      url: 'https://images.unsplash.com/photo-1555696972-863afbc72bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white boat on blue ocean water',
+      url: 'https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white canoe near palm tree',
+      url: 'https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'red and blue house near dock',
+      url: 'https://images.unsplash.com/photo-1490162190997-391cff4632e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'short-coated brown dog near seashore',
+      url: 'https://images.unsplash.com/photo-1555696972-863afbc72bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white boat on blue ocean water',
+      url: 'https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white canoe near palm tree',
+      url: 'https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'red and blue house near dock',
+      url: 'https://images.unsplash.com/photo-1490162190997-391cff4632e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'short-coated brown dog near seashore',
+      url: 'https://images.unsplash.com/photo-1555696972-863afbc72bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white boat on blue ocean water',
+      url: 'https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white canoe near palm tree',
+      url: 'https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'red and blue house near dock',
+      url: 'https://images.unsplash.com/photo-1490162190997-391cff4632e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'short-coated brown dog near seashore',
+      url: 'https://images.unsplash.com/photo-1555696972-863afbc72bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white boat on blue ocean water',
+      url: 'https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white canoe near palm tree',
+      url: 'https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'red and blue house near dock',
+      url: 'https://images.unsplash.com/photo-1490162190997-391cff4632e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'short-coated brown dog near seashore',
+      url: 'https://images.unsplash.com/photo-1555696972-863afbc72bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white boat on blue ocean water',
+      url: 'https://images.unsplash.com/photo-1484821582734-6c6c9f99a672?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'white canoe near palm tree',
+      url: 'https://images.unsplash.com/photo-1520520731457-9283dd14aa66?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'red and blue house near dock',
+      url: 'https://images.unsplash.com/photo-1490162190997-391cff4632e4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    },
+    {
+      title: 'short-coated brown dog near seashore',
+      url: 'https://images.unsplash.com/photo-1555696972-863afbc72bb0?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60'
+    }
+
+  ]
+  checkWindowIndex(index: number) {
+    return Math.abs(this.currentPage - index) < 5;
+  }
 }
